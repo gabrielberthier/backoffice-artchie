f34c391291a97a941636971d19fd5fcddcf2ca79
Gabriel Berthier <gnberthier@gmail.com>
2021-05-11 21:46:03 -0300

    test: assert new row is added

--- tests/Infrastructure/Persistence/ORM/Account/AccountTest.php
+++ tests/Infrastructure/Persistence/ORM/Account/AccountTest.php
@@ -6,8 +6,8 @@ namespace Tests\Infrastructure\Persistence\User;
 
 use App\Domain\Models\Account;
 use App\Domain\Repositories\AccountRepository;
-use DI\Container;
 use Doctrine\ORM\EntityManager;
+use function PHPUnit\Framework\assertSame;
 use Tests\TestCase;
 
 /**
@@ -17,22 +17,20 @@ use Tests\TestCase;
 class AccountTest extends TestCase
 {
     private AccountRepository $repository;
+    private EntityManager $entityManager;
 
     public function setUp(): void
     {
         $this->getAppInstance();
-        /** @var Container $container */
         $container = $this->getContainer();
-        // @var AccountRepository
+
         $this->repository = $container->get(AccountRepository::class);
+        $this->entityManager = $container->get(EntityManager::class);
     }
 
     protected function tearDown(): void
     {
-        /** @var Container $container */
-        $container = $this->getContainer();
-        /** @var EntityManager */
-        $entityManager = $container->get(EntityManager::class);
+        $entityManager = $this->entityManager;
         $collection = $entityManager->getRepository(Account::class)->findAll();
         foreach ($collection as $c) {
             $entityManager->remove($c);
@@ -45,5 +43,23 @@ class AccountTest extends TestCase
     {
         $account = new Account(email: 'mail.com', username: 'user', password: 'pass');
         $this->repository->insert($account);
+        $total = $this->getTotalCount();
+
+        assertSame($total, 1);
+    }
+
+    private function getTotalCount(): int
+    {
+        $qb = $this->entityManager->createQueryBuilder();
+
+        $qb->select($qb->expr()->count('u'))
+            ->from(Account::class, 'u')
+            // ->where('u.type = ?1')
+            // ->setParameter(1, 'employee')
+        ;
+
+        $query = $qb->getQuery();
+
+        return (int) $query->getSingleScalarResult();
     }
 }
