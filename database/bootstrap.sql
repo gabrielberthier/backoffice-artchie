2a0e2ae80ce4be3888d872c58c31d03e8c3f858b
Gabriel Berthier <gnberthier@gmail.com>
2021-09-08 22:12:07 -0300

    feat: add insertion using bootstrap sql file

---
+++ database/bootstrap.sql
@@ -0,0 +1,20 @@
+insert into
+  accounts(
+    email,
+    username,
+    password,
+    role,
+    uuid,
+    created_at,
+    updated_at
+  )
+values
+  (
+    'adming@arcthie.com',
+    'admin',
+    '$2y$08$xowZtJwq2dG5FX0eA7rIaukC2WnBoHcLMSzJH090.OyjLbLBLar7G',
+    'admin',
+    '0c04d7f5-5e42-4fdd-9ba2-d1b44cd22ac9',
+    now(),
+    now()
+  )
