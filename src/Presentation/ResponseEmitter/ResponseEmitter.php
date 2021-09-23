0270d912d70dc9ca757d8bc4bce75fe14908c9e7
Gabriel Berthier <gnberthier@gmail.com>
2021-09-22 22:32:46 -0300

    fix: set cors

--- src/Presentation/ResponseEmitter/ResponseEmitter.php
+++ src/Presentation/ResponseEmitter/ResponseEmitter.php
@@ -19,6 +19,7 @@ class ResponseEmitter extends SlimResponseEmitter
 
         $response = $response
             ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
