d73874980ad6ed7a0383a5313b2ee5797492e33b
Gabriel Berthier <gnberthier@gmail.com>
2021-11-22 09:54:08 -0300

    feat: add trailing slash middleware

--- app/HTTP/Middlewares/Middleware.php
+++ app/HTTP/Middlewares/Middleware.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Core\HTTP\Middlewares;
 
 use Core\ResourceLoader;
+use Middlewares\TrailingSlash;
 use Slim\App;
 
 class Middleware
@@ -14,6 +15,7 @@ class Middleware
         // Defaults
         $app->addBodyParsingMiddleware(); // Add parser that handles body values
         $app->addRoutingMiddleware(); // Add the Slim built-in routing middleware
+        $app->add(new TrailingSlash());
 
         // Apply middlewares
         $definitions = ResourceLoader::getResource('middlewares');
