ad568fbd5064711fb7f966e3a7882005c3bf1822
Gabriel Nogueira Berthier da Silva <gabriel.berthier@vocedm.com.br>
2023-08-31 12:55:30 -0300

    refactor: remove unused files, enhance code quality

--- cli-config.php
+++ cli-config.php
@@ -15,7 +15,7 @@ $containerFactory = new ContainerFactory();
 
 $container = $containerFactory->get();
 
-$config = new PhpFile('migrations.php');
+$config = new PhpFile('./app/definitions/doctrine-migrations.php');
 
 $em = $container->get(EntityManager::class);
 
